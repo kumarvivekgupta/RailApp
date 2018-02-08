2d0464466b4fe454606268c9442f891ec1a31989
NaimishVerma17 <naimishverma50@gmail.com>
2018-02-08 23:10:04 +0530

    file structure created

---
+++ app/src/main/java/com/test/naimish/railapp/Activities/SingleFragmentActivity.java
@@ -0,0 +1,15 @@
+package com.test.naimish.railapp.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.test.naimish.railapp.R;
+
+public class SingleFragmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_fragment);
+    }
+}
