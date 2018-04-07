9ffd2931fce3daf12fd9244dc76aba00d2fff2e6
NaimishVerma17 <naimishverma50@gmail.com>
2018-04-07 23:29:36 +0530

    activity changes

--- app/src/main/java/com/test/naimish/railapp/Fragments/SettingsFragment.java
+++ app/src/main/java/com/test/naimish/railapp/Fragments/SettingsFragment.java
@@ -1,12 +1,16 @@
 package com.test.naimish.railapp.Fragments;
 
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
 import android.widget.EditText;
 
 import com.test.naimish.railapp.Activities.ChangePasswordActivity;
 import com.test.naimish.railapp.R;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 /**
@@ -46,5 +50,10 @@ public class SettingsFragment extends RailAppFragment {
 
     public static SettingsFragment newInstance() {
         return new SettingsFragment();
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
