343f6da9217252ed6937dbea4ac01dd8748fb480
naimishverma17 <naimishverma50@gmail.com>
2018-04-13 22:32:11 +0530

    interface added

--- app/src/main/java/com/test/naimish/railapp/Network/UpdateProfileNetwork/UpdateProfileInterface.java
+++ app/src/main/java/com/test/naimish/railapp/Network/UpdateProfileNetwork/UpdateProfileInterface.java
@@ -2,9 +2,14 @@ package com.test.naimish.railapp.Network.UpdateProfileNetwork;
 
 //Created by naimish on 13/4/18
 
+import com.test.naimish.railapp.Models.UpdateProfile;
+
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public interface UpdateProfileInterface {
 
+    @POST("/users/update-profile")
+    Call<UpdateProfile> updateProfile(@Body UpdateProfile updateProfile);
 }
