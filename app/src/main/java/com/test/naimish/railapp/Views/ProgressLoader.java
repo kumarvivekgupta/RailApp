4ed55fc71fd5a3145274d21d3b58dccb1f03a2cb
naimishverma17 <naimishverma50@gmail.com>
2018-05-02 22:52:25 +0530

    small changes

--- app/src/main/java/com/test/naimish/railapp/Views/ProgressLoader.java
+++ app/src/main/java/com/test/naimish/railapp/Views/ProgressLoader.java
@@ -8,20 +8,22 @@ import com.test.naimish.railapp.Activities.SingleFragmentActivity;
 public class ProgressLoader {
     private Context activity;
     KProgressHUD kProgressHUD;
-    public ProgressLoader(Context activity){
-        this.activity=activity;
+
+    public ProgressLoader(Context activity) {
+        this.activity = activity;
     }
 
 
     public void showLoader() {
-        kProgressHUD=KProgressHUD.create(activity)
+        kProgressHUD = KProgressHUD.create(activity)
                 .setStyle(KProgressHUD.Style.SPIN_INDETERMINATE)
                 .setCancellable(true)
                 .setAnimationSpeed(2)
                 .setDimAmount(0.5f)
                 .show();
     }
-    public void dismissLoader(){
+
+    public void dismissLoader() {
         kProgressHUD.dismiss();
     }
 
